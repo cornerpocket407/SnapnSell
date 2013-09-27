7ac340770f04350610282fc81631ff114a7a7766
Matthew Lee <Matt@wsip-98-173-91-178.lv.lv.cox.net>
2013-09-27 13:44:25 -0700

    Added Activities for Login, Filter and Marketplace. Created the layouts for Login and Filter.

--- gen/com/snapnsell/R.java
+++ gen/com/snapnsell/R.java
@@ -24,29 +24,57 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int btnSubmit=0x7f080008;
-        public static final int etDescription=0x7f080006;
-        public static final int etLocation=0x7f080007;
-        public static final int etPrice=0x7f080004;
+        public static final int action_settings=0x7f080015;
+        public static final int btnLogin=0x7f08000d;
+        public static final int btnSubmit=0x7f080014;
+        public static final int button1=0x7f08000b;
+        public static final int cbUsedFilter=0x7f08000a;
+        public static final int etDescription=0x7f080012;
+        public static final int etLocation=0x7f080013;
+        public static final int etLocationInput=0x7f080007;
+        public static final int etMaxPriceInput=0x7f080009;
+        public static final int etMinPriceInput=0x7f080008;
+        public static final int etPrice=0x7f080011;
         public static final int frame_container=0x7f080000;
-        public static final int imageView1=0x7f080001;
-        public static final int textView1=0x7f080005;
-        public static final int tvDescription=0x7f080002;
-        public static final int tvPrice=0x7f080003;
+        public static final int imageView1=0x7f08000e;
+        public static final int ivProfile=0x7f08000c;
+        public static final int spCategoryFilter=0x7f080006;
+        public static final int tvCategoryFilterLabel=0x7f080001;
+        public static final int tvDescription=0x7f08000f;
+        public static final int tvIsUsedFilterLabel=0x7f080005;
+        public static final int tvLocationFilterLabel=0x7f080002;
+        public static final int tvMaxPriceFilterLabel=0x7f080004;
+        public static final int tvMinPriceFilterLabel=0x7f080003;
+        public static final int tvPrice=0x7f080010;
     }
     public static final class layout {
         public static final int activity_buy=0x7f030000;
-        public static final int fragment_item=0x7f030001;
+        public static final int activity_filter=0x7f030001;
+        public static final int activity_login=0x7f030002;
+        public static final int activity_marketplace=0x7f030003;
+        public static final int fragment_item=0x7f030004;
     }
     public static final class menu {
         public static final int buy=0x7f070000;
+        public static final int filter=0x7f070001;
+        public static final int login=0x7f070002;
+        public static final int marketplace=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int btnSubmit=0x7f050007;
+        public static final int filter_category_label=0x7f05000c;
+        public static final int filter_isused_label=0x7f050010;
+        public static final int filter_location_label=0x7f05000d;
+        public static final int filter_maxprice_label=0x7f05000f;
+        public static final int filter_minprice_label=0x7f05000e;
+        public static final int filter_save_btn=0x7f050011;
         public static final int hello_world=0x7f050002;
+        public static final int login_btn_label=0x7f05000b;
+        public static final int title_activity_filter=0x7f050009;
+        public static final int title_activity_login=0x7f050008;
+        public static final int title_activity_marketplace=0x7f05000a;
         public static final int tvDescription=0x7f050004;
         public static final int tvItem=0x7f050003;
         public static final int tvLocation=0x7f050006;
